6828344c62cc0156e8fff0ca823ae21e178ea12f
dayeon-alert <dayeon.alert@gmail.com>
2022-10-20 00:35:16 +0900

    불필요한 useState 삭제 및 유효성 검사 개선

--- Client/src/pages/SignUp/SignUp.jsx
+++ Client/src/pages/SignUp/SignUp.jsx
@@ -1,21 +1,20 @@
-import React, { useState } from 'react'
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
-import CustomPaper from '../../components/CustomPaper';
-import { useTheme } from '@mui/material/styles';
-import styled from '@emotion/styled';
+import React, { useState, useEffect } from 'react'
 import axios from "axios";
+import styled from '@emotion/styled';
+import CustomPaper from '../../components/CustomPaper';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 
 const url = 'https://b0055c23-9249-422f-a317-960947120571.mock.pstmn.io';
 
 function CustomFormControlLabel(props){
   return (
     <FormControlLabel 
-      control={<Checkbox className = "Checkbox" value={props.controlvalue}/>}
+      control={<Checkbox className = "Checkbox"/>}
       label={
         <Typography variant="body1">
           {props.labeltext}
@@ -26,28 +25,15 @@ function CustomFormControlLabel(props){
   );
 }
 
-const StyledFormControlLabel = styled(CustomFormControlLabel)`
-  & .Checkbox {
-    padding: ${props => props.theme.spacing(0.5, 1)};
-    & .MuiSvgIcon-root {
-      font-size: 1.2rem;
-    }
-  }   
-`;
-
 function CustomTextField(props){
   return (
-    <TextField variant="outlined" size="small" error={props.usererrcode>-1?false:true} 
-    helperText={handleHelperText(props.usererrcode)} {...props}
+    <TextField variant="outlined" size="small" error={props.helpermsgcode>-1?false:true} 
+    helperText={handleHelperText(props.helpermsgcode)} {...props}
     />
   );
 }
 
-const StyledTextField = styled(CustomTextField)`
-  & .MuiInputBase-input {
-    padding: ${props => props.theme.spacing(0.5, 2)};
-  }
-`;
+
 
 /*
 1. 별명은 1자 이상 30자 미만으로 입력해주세요.
@@ -56,32 +42,40 @@ const StyledTextField = styled(CustomTextField)`
 4. 사용 중인 별명입니다.
 5. 사용 가능한 별명입니다.
 */
-function handleHelperText(usererrcode) {
-  if(usererrcode===0) return "";
-  else if(usererrcode===100) return "별명을 입력해주세요.";
-  else if(usererrcode===200) return "30자 이하로만 입력 가능합니다.";
-  else if(usererrcode===300) return "특수문자는 @#~만 입력해주세요.";
-  else if(usererrcode===400) return "중복 확인을 해주세요.";
-  else if(usererrcode===500) return "사용 중인 별명입니다.";
-  else if(usererrcode===600) return "사용 가능한 별명입니다.";
+function handleHelperText(msgCode) {
+  if(msgCode===0) return "";
+  else if(msgCode===100) return "사용 가능한 별명입니다.";
+  else if(msgCode===101) return "별명을 입력해주세요.";
+  else if(msgCode===102) return "30자 미만으로 입력해주세요.";
+  else if(msgCode===103) return "특수문자는 @#~만 입력해주세요.";
+  else if(msgCode===104) return "중복 확인을 해주세요.";
+  else if(msgCode===105) return "사용 중인 별명입니다.";
 }
 
-export default function SignUp() {
-  const theme = useTheme();
+const checkData = [
+  { 
+    labelText: "이용약관 동의",
+    isChecked: false
+  },
+  { 
+    labelText: "개인정보 수집 및 이용 동의",
+    isChecked: false
+  }
+]
 
-  const [userNickname, setUserNickname] = useState('')
-  const [userNicknameErrCode, setUserNicknameErrCode] = useState(0)
-  const [userEmail, setUserEmail] = useState('')
-  const [userEmailErrCode, setUserEmailErrCode] = useState(0)
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  const [allowAllterms, setAllowAllterms] = useState(false)
-  const [allowServiceTerms, setAllowServiceTerms] = useState(false)
-  const [allowPrivateTerms, setAllowPrivateTerms] = useState(false)
+export default function SignUp() {
+  const [userNicknameMsgCode, setUserNicknameMsgCode] = useState(101);
+  const [userEmailErrCode, setUserEmailErrCode] = useState(0);
+  const [allowTerms, setAllowTerms] = useState(checkData);
+
+  useEffect(() => {
+    setAllowTerms(checkData);
+  }, []);
 
   const handleSubmit = (event) => {
+    const { currentTarget } = event;
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
+    const data = new FormData(currentTarget);
     if(!(data.get('userNickname')
       && data.get('email')
       && data.get('password')
@@ -95,20 +89,40 @@ export default function SignUp() {
     });
   };
 
+  const handleNicknameInput = (e) => {
+    const userNickname = e.target.value;
+
+    // 중복 확인 버튼 활성화되어 있을 경우 비활성화
+    if(userNickname.length < 1) {
+      setUserNicknameMsgCode(101);
+    }
+    else if(userNickname.length > 29){
+      setUserNicknameMsgCode(102);
+    }
+    else{
+      // 중복 확인 버튼 활성화
+      setUserNicknameMsgCode(104);
+    }
+  }
+
   function handleUserNickname() {
-    axios.get(`${url}/api/v1/nicknames/duplicate`).then((response)=> {
-      if(response.data.user_is_unique){
-        setUserEmailErrCode(1);
+    axios.get(`${url}/api/v1/nicknames/duplicate`).then((response) => {
+      const { nickname_is_unique } = response.data
+      if(nickname_is_unique) {
+        setUserNicknameMsgCode(100);
+        // 중복 확인 버튼 비활성화
+        // 다음 입력창으로 focus 이동
       }
       else{
-        setUserEmailErrCode(-1);
+        setUserNicknameMsgCode(105);
       }
     })
   }
 
   function handleUserEmail() {
     axios.get(`${url}/api/v1/users/duplicate`).then((response)=> {
-      if(response.data.nickname_is_unique){
+      const { user_is_unique } = response.data
+      if(user_is_unique){
         setUserEmailErrCode(1);
       }
       else{
@@ -117,8 +131,12 @@ export default function SignUp() {
     }) 
   }
 
+  const handleChange = () => {
+    // Component와 연결된 isChecked를 true 혹은 false로 변경한다.
+  }
+
   return (
-    <CustomPaper sx={{gap: theme.spacing(5, 0)}}>
+    <StyledPaper>
       <Typography variant="h5">
           회원가입
       </Typography>
@@ -126,38 +144,25 @@ export default function SignUp() {
         <Stack spacing={1}>
           <Stack direction="row" alignItems="flex-start" spacing={1}>
             <StyledTextField
-              value={userNickname}
-              onInput={(e) => {
-                if(e.target.value.length < 1) {
-                  setUserNicknameErrCode(100);
-                  setUserNickname(e.target.value);
-                }
-                else if(e.target.value.length > 30){
-                  setUserNicknameErrCode(200);
-                }
-                else{
-                  setUserNicknameErrCode(0);
-                  setUserNickname(e.target.value);
-                }
-              }}
+              onInput={handleNicknameInput}
               placeholder="별명"
               name="userNickname"
-              usererrcode={userNicknameErrCode}
+              helpermsgcode={userNicknameMsgCode}
               autoComplete="off"
+              inputProps={{maxLength:30}}
             />
             <Button
               size="small"
               variant="contained"
               color="primary"
               onClick={handleUserNickname}
+              disabled={true}
             >
               중복확인
             </Button>
           </Stack>
           <Stack direction="row" alignItems="flex-start" spacing={1}>
             <StyledTextField
-              value={userEmail}
-              onInput={(e) => setUserEmail(e.target.value)}
               placeholder="이메일"
               name="email"
               usererrcode={userEmailErrCode}
@@ -173,16 +178,12 @@ export default function SignUp() {
             </Button>
           </Stack>
           <StyledTextField
-            value={password}
-            onInput={(e) => setPassword(e.target.value)}
             name="password"
             placeholder="비밀번호"
             type="password"
             autoComplete="password"
           />
           <StyledTextField
-            value={confirmPassword}
-            onInput={(e) => setConfirmPassword(e.target.value)}
             name="confirmPassword"
             placeholder="비밀번호 확인"
             type="password"
@@ -193,38 +194,56 @@ export default function SignUp() {
           <StyledFormControlLabel
           controlvalue = "allowAllTerms"
           labeltext = "전체 동의"
-          checked={allowAllterms}
-          onChange={(e)=>{
-            setAllowAllterms(e.target.checked);
-            setAllowServiceTerms(e.target.checked);
-            setAllowPrivateTerms(e.target.checked);
-          }}
-          theme={theme}
+          onChange={handleChange}
           />
-          <StyledFormControlLabel
-            controlvalue = "allowServiceTerms"
-            labeltext = "이용약관 동의"
-            checked={allowServiceTerms}
-            onChange={(e)=> setAllowServiceTerms(e.target.checked)}
-          />
-          <StyledFormControlLabel
-            controlvalue = "allowPrivateTerms"
-            labeltext = "개인정보 수집 및 이용 동의"
-            checked={allowPrivateTerms}
-            onChange={(e)=> setAllowPrivateTerms(e.target.checked)}
-          />
+          {
+            allowTerms.map((allowTerm)=> (
+              <StyledFormControlLabel
+                key = {allowTerm.labelText}
+                labeltext = {allowTerm.labelText}
+                checked = {allowTerm.isChecked}
+                onChange = {handleChange}
+              />
+            ))
+          }
         </Stack>
-        <Button
+        <StyledButton
           size="small"
           type="submit"
           variant="contained"
           color="primary"
           fullWidth
-          sx={{paddingTop:1.2, paddingBottom:1.2}}
         >
           회원가입
-        </Button>
+        </StyledButton>
       </Stack>
-    </CustomPaper>
+    </StyledPaper>
   )
-} 
+}
+
+const StyledButton = styled(Button)`
+  padding: ${props => props.theme.spacing(5)}, 0;
+`;
+
+const StyledFormControlLabel = styled(CustomFormControlLabel)`
+  & .Checkbox {
+    padding: ${props => props.theme.spacing(0.5, 1)};
+    & .MuiSvgIcon-root {
+      font-size: 1.2rem;
+    }
+  }   
+`;
+
+const StyledTextField = styled(CustomTextField)`
+  & .MuiInputBase-input {
+    padding: ${props => props.theme.spacing(0.5, 2)};
+  }
+`;
+
+const StyledPaper = styled(CustomPaper)`
+  & > * {
+    :not(:last-child){
+      margin-bottom: ${props=> props.theme.spacing(4)};
+    }
+  }
+`;
