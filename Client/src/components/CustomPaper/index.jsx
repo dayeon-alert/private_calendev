f0edb0330756ed546611af5554c5f7741947f79a
dayeon-alert <dayeon.alert@gmail.com>
2022-10-20 00:33:28 +0900

    Edit CustomPaper Component style

--- Client/src/components/CustomPaper/index.jsx
+++ Client/src/components/CustomPaper/index.jsx
@@ -1,19 +1,17 @@
 import Paper from '@mui/material/Paper'
 import { styled } from '@mui/material'
 
-const CustomPaper = styled(Paper)(({ theme }) => {
-  return {
-    width: 'auto',
-    margin: theme.spacing(8, 2, 'auto'),
-    [theme.breakpoints.up('sm')]: {
-      width: 400,
-      margin: theme.spacing(8, 'auto', 'auto'),
-    },
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: theme.spacing(5),
+const CustomPaper = styled(Paper)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: auto;
+  padding: ${props => props.theme.spacing(4, 1)};
+  margin: ${props=>props.theme.spacing(8, 1, 'auto')};
+  ${props => props.theme.breakpoints.up('sm')} {
+    width: 400px;
+    margin: ${props => props.theme.spacing(8, 'auto', 'auto')};
   }
-})
+`
 
 export default CustomPaper 
