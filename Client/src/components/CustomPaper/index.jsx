7290dd661dc8baedb03b59ede76a8e0ca83c832a
Dayeon <dayeon.alert@gmail.com>
2022-10-21 08:02:34 +0900

    Edit Custompaper Component design padding

--- Client/src/components/CustomPaper/index.jsx
+++ Client/src/components/CustomPaper/index.jsx
@@ -6,7 +6,7 @@ const CustomPaper = styled(Paper)`
   flex-direction: column;
   align-items: center;
   width: auto;
-  padding: ${props => props.theme.spacing(4, 1)};
+  padding: ${props => props.theme.spacing(4, 2)};
   margin: ${props=>props.theme.spacing(12, 1, 'auto')};
   ${props => props.theme.breakpoints.up('sm')} {
     width: 400px;
