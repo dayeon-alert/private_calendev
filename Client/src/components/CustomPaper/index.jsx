8f3bf1a281bc6f733525dc6c27d11d9a71ff8271
dayeon-alert <dayeon.alert@gmail.com>
2022-10-23 22:52:02 +0900

    CustomPaper 컴포넌트 레이아웃 스타일 수정

--- Client/src/components/CustomPaper/index.jsx
+++ Client/src/components/CustomPaper/index.jsx
@@ -1,17 +1,17 @@
-import Paper from '@mui/material/Paper'
-import { styled } from '@mui/material'
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material';
 
 const CustomPaper = styled(Paper)`
   display: flex;
   flex-direction: column;
   align-items: center;
   width: auto;
-  padding: ${props => props.theme.spacing(4, 2)};
-  margin: ${props=>props.theme.spacing(12, 1, 'auto')};
-  ${props => props.theme.breakpoints.up('sm')} {
+  padding: ${(props) => props.theme.spacing(4, 1)};
+  margin: ${(props) => props.theme.spacing(12, 1, 'auto')};
+  ${(props) => props.theme.breakpoints.up('sm')} {
     width: 400px;
-    margin: ${props => props.theme.spacing(12, 'auto', 'auto')};
+    margin: ${(props) => props.theme.spacing(12, 'auto', 'auto')};
   }
-`
+`;
 
-export default CustomPaper 
+export default CustomPaper;
