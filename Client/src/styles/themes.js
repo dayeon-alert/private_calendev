e09db3bd0aa5f63ae344a6ce0d12f12a6be432f7
dayeon-alert <dayeon.alert@gmail.com>
2022-10-28 00:27:10 +0900

    Global Style Breakpoints에 Device 추가

--- Client/src/styles/themes.js
+++ Client/src/styles/themes.js
@@ -11,6 +11,19 @@ const themes = {
       main: red[300],
     },
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+      mobile: 480,
+      tablet: 768,
+      laptop: 1024,
+      desktop: 1200,
+    },
+  },
 };
 
 export default themes;
